ed1d50222ee2f0932ed35f50c3f99cb001264434
frohikey <frohikey@gmail.com>
2017-09-16 17:32:14 +0200

    Add DocumentDb configuration

---
+++ KotoriCore/Configuration/DocumentDb.cs
@@ -0,0 +1,32 @@
+﻿namespace KotoriCore.Configuration
+{
+    /// <summary>
+    /// Document Db connection string.
+    /// </summary>
+    public class DocumentDb
+    {
+        /// <summary>
+        /// Gets or sets the end point.
+        /// </summary>
+        /// <value>The end point.</value>
+        public string EndPoint { get; set; }
+
+        /// <summary>
+        /// Gets or sets the authorization key.
+        /// </summary>
+        /// <value>The authorization key.</value>
+        public string AuthorizationKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the database.
+        /// </summary>
+        /// <value>The database.</value>
+        public string Database { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection.
+        /// </summary>
+        /// <value>The collection.</value>
+        public string Collection { get; set; }
+    }
+}
