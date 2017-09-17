7fa2673ca6dce792495438bec0079139fec7a570
frohikey <frohikey@gmail.com>
2017-09-17 15:00:28 +0200

    Refactor service bus configuration

--- KotoriCore/Configuration/ServiceBusConfiguration.cs
+++ KotoriCore/Configuration/ServiceBusConfiguration.cs
@@ -5,29 +5,28 @@
     /// </summary>
     public class ServiceBusConfiguration : IBusConfiguration
     {
-		/// <summary>
-		/// Gets or sets the end point.
-		/// </summary>
-		/// <value>The end point.</value>
-		public string Endpoint { get; set; }
+        /// <summary>
+        /// Gets or sets the end point.
+        /// </summary>
+        /// <value>The end point.</value>
+        public string Endpoint { get; set; }
 
         /// <summary>
-        /// Gets or sets the shared secret issuer.
+        /// Gets or sets the name of the shared access key.
         /// </summary>
-        /// <value>The shared secret issuer.</value>
-        public string SharedSecretIssuer { get; set; }
+        /// <value>The name of the shared access key.</value>
+        public string SharedAccessKeyName { get; set; }
 
         /// <summary>
-        /// Gets or sets the shared secret value.
+        /// Gets or sets the shared access key.
         /// </summary>
-        /// <value>The shared secret value.</value>
-        public string SharedSecretValue { get; set; }
+        /// <value>The shared access key.</value>
+        public string SharedAccessKey { get; set; }
 
         /// <summary>
-        /// Gets or sets the queue name.
+        /// Gets or sets the entity path.
         /// </summary>
-        /// <value>The queue name.</value>
-        public string Queue { get; set; }
-	
+        /// <value>The entity path.</value>
+        public string EntityPath { get; set; }
     }
 }
