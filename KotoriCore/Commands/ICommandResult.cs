20fe0d0ade03c7d06319353b3307f85c07852a32
frohikey <frohikey@gmail.com>
2017-09-21 22:23:50 +0200

    Clean ups

--- KotoriCore/Commands/ICommandResult.cs
+++ KotoriCore/Commands/ICommandResult.cs
@@ -3,10 +3,27 @@ using System.Collections;
 
 namespace KotoriCore.Commands
 {
+    /// <summary>
+    /// Command result interface.
+    /// </summary>
     public interface ICommandResult
     {        
+        /// <summary>
+        /// Gets the type of the element.
+        /// </summary>
+        /// <value>The type of the element.</value>
         Type ElementType { get; }
+
+        /// <summary>
+        /// Gets the data.
+        /// </summary>
+        /// <value>The data.</value>
         IEnumerable Data { get; }
+
+        /// <summary>
+        /// Gets the message.
+        /// </summary>
+        /// <value>The message.</value>
         string Message { get; }
     }
 }
