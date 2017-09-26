10b5082811dea3b7a52b47acd9af3f9fbe4be5a9
frohikey <frohikey@gmail.com>
2017-09-27 00:26:59 +0200

    Basic YAML tests just before zzz

--- KotoriCore.Tests/Kotori.cs
+++ KotoriCore.Tests/Kotori.cs
@@ -6,6 +6,10 @@ using KotoriCore.Exceptions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using KotoriCore.Helpers;
+using YamlDotNet.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Sushi2;
 
 namespace KotoriCore.Tests
 {
@@ -13,6 +17,7 @@ namespace KotoriCore.Tests
     public class Project
     {
         Kotori _kotori;
+        string _basePath;
 
         [TestInitialize]
         public void Init()
@@ -25,6 +30,7 @@ namespace KotoriCore.Tests
                 .Build();
 
             _kotori = new Kotori(appSettings);
+            _basePath = appSettings["Kotori.Tests:SampleDataPath"];
         }
 
         [TestCleanup]
@@ -98,6 +104,28 @@ namespace KotoriCore.Tests
             projects = results.ToDataList<Domains.SimpleProject>();
 
             Assert.AreEqual(0, projects.Count());
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var content = @"
+name: Ami Kawashima (川嶋 亜美)
+height: 165 cm
+weight: 45 kg
+bwh: { b: 90, w: 58, h: 88 }";
+            var r = new StringReader(content);
+            var deserializer = new DeserializerBuilder().Build();
+            var yamlObject = deserializer.Deserialize(r);
+            var serializer = new SerializerBuilder()
+                .JsonCompatible()
+                .Build();
+
+            var json = serializer.Serialize(yamlObject);
+            dynamic d = JObject.Parse(json);
+
+            Assert.AreEqual("Ami Kawashima (川嶋 亜美)", d.name.ToString());
+            //Assert.AreEqual(58, d.bwh.w.ToInt32() ?? 0);
         }
     }
 }
