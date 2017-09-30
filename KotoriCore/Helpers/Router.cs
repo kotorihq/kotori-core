45cf104c8898d1388f37ae1da7d4bf1be328d5c1
frohikey <frohikey@gmail.com>
2017-09-30 23:29:36 +0200

    Add router Update libs

---
+++ KotoriCore/Helpers/Router.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using KotoriCore.Exceptions;
+
+namespace KotoriCore.Helpers
+{
+    /// <summary>
+    /// Router.
+    /// </summary>
+    public static class Router
+    {
+        /// <summary>
+        /// Convert id to kotori uri.
+        /// </summary>
+        /// <returns>The kotori URI.</returns>
+        /// <param name="uri">URI.</param>
+        public static Uri ToKotoriUri(this string uri)
+        {
+            if (!Uri.TryCreate(uri, UriKind.Relative, out Uri result))
+            {
+                throw new KotoriValidationException("Identifier is not valid URI string.");
+            }
+
+            return result;
+        }
+    }
+}
