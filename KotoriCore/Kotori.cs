fd3db2c868931cb85d97bc904ff5dd4bcad0efe1
frohikey <frohikey@gmail.com>
2017-10-02 22:54:01 +0200

    New cute kotori interface

--- KotoriCore/Kotori.cs
+++ KotoriCore/Kotori.cs
@@ -5,6 +5,7 @@ using KotoriCore.Database.DocumentDb;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace KotoriCore
 {
@@ -56,11 +57,52 @@ namespace KotoriCore
         }
 
         /// <summary>
-        /// Process the specified command.
+        /// Upserts document.
         /// </summary>
-        /// <returns>The process.</returns>
-        /// <param name="command">Command.</param>
-        public async Task<ICommandResult> ProcessAsync(ICommand command) 
+        /// <param name="instance">Instance.</param>
+        /// <param name="projectId">Project identifier.</param>
+        /// <param name="identifier">Identifier.</param>
+        /// <param name="content">Content.</param>
+        /// <returns>Result.</returns>
+        public async Task<UpsertDocument> UpsertDocumentAsync(string instance, string projectId, string identifier, string content)
+        {
+            return await ProcessAsync(new UpsertDocument(instance, projectId, identifier, content)) as UpsertDocument;
+        }
+
+        /// <summary>
+        /// Creates project.
+        /// </summary>
+        /// <param name="instance">Instance.</param>
+        /// <param name="name">Name.</param>
+        /// <param name="projectId">Identifier.</param>
+        /// <param name="projectKeys">Project keys.</param>
+        /// <returns>Result.</returns>
+        public async Task<CreateProject> CreateProjectAsync(string instance, string name, string projectId, IEnumerable<ProjectKey> projectKeys)
+        {
+            return await ProcessAsync(new CreateProject(instance, name, projectId, projectKeys)) as CreateProject;
+        }
+
+        /// <summary>
+        /// Deletes project.
+        /// </summary>
+        /// <param name="instance">Instance.</param>
+        /// <param name="identifier">Identifier.</param>
+        /// <returns>Result.</returns>
+        public async Task<DeleteProject> DeleteProjectAsync(string instance, string identifier)
+        {
+            return await ProcessAsync(new DeleteProject(instance, identifier)) as DeleteProject;
+        }
+        /// <summary>
+        /// Get projects.
+        /// </summary>
+        /// <param name="instance">Instance.</param>
+        /// <returns>Result.</returns>
+        public async Task<GetProjects> GetProjectsAsync(string instance)
+        {
+            return await ProcessAsync(new GetProjects(instance)) as GetProjects;    
+        }
+
+        internal async Task<ICommandResult> ProcessAsync(ICommand command)
         {
             return await _database.HandleAsync(command);
         }
