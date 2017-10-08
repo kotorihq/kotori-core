e249e39f341c9c87ecb49374060f5fef23e6118a
frohikey <frohikey@gmail.com>
2017-10-08 21:58:04 +0200

    Claim enum moved back to server, close #32

--- KotoriCore/Helpers/Enums.cs
+++ KotoriCore/Helpers/Enums.cs
@@ -6,21 +6,6 @@
     public static class Enums
     {
         /// <summary>
-        /// Claim types.
-        /// </summary>
-        public enum ClaimType
-        {
-            /// <summary>
-            /// The master.
-            /// </summary>
-            Master = 0,
-            /// <summary>
-            /// The project.
-            /// </summary>
-            Project = 1
-        }
-
-        /// <summary>
         /// Document types.
         /// </summary>
         public enum DocumentType
