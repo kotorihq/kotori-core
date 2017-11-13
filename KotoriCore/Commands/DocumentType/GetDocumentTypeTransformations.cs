f201f716efbf464cd7eb28bcd76d6a642d0f052b
frohikey <frohikey@gmail.com>
2017-11-13 21:59:23 +0100

    Refactor GetDocumentTypeTransformations - Identifier to DocumentId, close #108

--- KotoriCore/Commands/DocumentType/GetDocumentTypeTransformations.cs
+++ KotoriCore/Commands/DocumentType/GetDocumentTypeTransformations.cs
@@ -3,28 +3,37 @@ using KotoriCore.Helpers;
 
 namespace KotoriCore.Commands
 {
+    /// <summary>
+    /// Get document type transformations command.
+    /// </summary>
     public class GetDocumentTypeTransformations : Command
     {
         /// <summary>
-        /// Gets the instance.
+        /// The instance.
         /// </summary>
         public readonly string Instance;
 
         /// <summary>
-        /// Gets the project identifier.
+        /// The project identifier.
         /// </summary>
         public readonly string ProjectId;
 
         /// <summary>
-        /// Gets the identifier.
+        /// The document type identifier.
         /// </summary>
-        public readonly string Identifier;
+        public readonly string DocumentTypeId;
 
-        public GetDocumentTypeTransformations(string instance, string projectId, string identifier)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:KotoriCore.Commands.GetDocumentTypeTransformations"/> class.
+        /// </summary>
+        /// <param name="instance">Instance.</param>
+        /// <param name="projectId">Project identifier.</param>
+        /// <param name="documentTypeId">Document type identifier.</param>
+        public GetDocumentTypeTransformations(string instance, string projectId, string documentTypeId)
         {
             Instance = instance;
             ProjectId = projectId;
-            Identifier = identifier;
+            DocumentTypeId = documentTypeId;
         }
 
         /// <summary>
@@ -39,8 +48,8 @@ namespace KotoriCore.Commands
             if (string.IsNullOrEmpty(ProjectId))
                 yield return new ValidationResult("Project Id must be set.");
 
-            if (string.IsNullOrEmpty(Identifier))
-                yield return new ValidationResult("Identifier must be set.");
+            if (string.IsNullOrEmpty(DocumentTypeId))
+                yield return new ValidationResult("Document type Id must be set.");
         }
     }
 }
