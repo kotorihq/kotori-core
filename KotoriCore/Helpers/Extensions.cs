7ba7a46027441bf8345b65ff040301b935278580
frohikey <frohikey@gmail.com>
2018-04-01 16:57:29 +0200

    Update document type hash extension, close #207

--- KotoriCore/Helpers/Extensions.cs
+++ KotoriCore/Helpers/Extensions.cs
@@ -106,7 +106,7 @@ namespace KotoriCore.Helpers
                 throw new ArgumentNullException(nameof(documentType));
 
             var c = documentType.Type +
-                documentType.Indexes?.Select(i => i.From + "-" + i.To).ToImplodedString() +
+                documentType.Indexes?.Select(i => i.From + "-" + (i.To == null ? "null" : ((int)i.To).ToString()) + "-" + i.IsRequired + "-" + i.Type).ToImplodedString() +
                 documentType.Transformations?.Select(t => t.From + "-" + t.To + "-" + t.Transformations.Select(t2 => t2.ToString())?.ToImplodedString())?.ToImplodedString();
 
             return HashTools.GetHash(c, HashTools.HashType.SHA1);
