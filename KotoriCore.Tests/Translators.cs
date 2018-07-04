166fd811b8d152f60a7dc18f11409c9d017f6637
Joey Frohikey <frohikey@gmail.com>
2018-07-04 17:53:43 +0200

    Test prep

--- KotoriCore.Tests/Translators.cs
+++ KotoriCore.Tests/Translators.cs
@@ -101,5 +101,85 @@ namespace KotoriCore.Tests
             var sqlQuery = documentTranslator.Translate(query);
             Assert.AreEqual("select count(1)  from c where c.title = 'moeta' and c.property.field <> 'val' or c.number >= 5 and (c.likes <> 3 or c.special < 3) and c.entity = 'kotori/document' and c.instance = 'dev'  and c.projectId = 'kotori://api/projects/testproject' and c.documentTypeId = 'kotori://api/projects/testproject/content/articles' ", sqlQuery);
         }
+
+        [TestMethod]
+        public void DocumentSelectSample()
+        {
+            var query = new ComplexQuery(
+                "id, robot",
+                "title eq 'moeta'",
+                30,
+                null,
+                null,
+                "dev",
+                false);
+            var documentTranslator = new DocumentTranslator("test", Helpers.Enums.DocumentType.Data, "records", 3);
+            var sqlQuery = documentTranslator.Translate(query);
+            Assert.AreEqual("select top 30 c.identifier, c.robot from c where c.title = 'moeta' and c.entity = 'kotori/project' and c.instance = 'dev' ", sqlQuery);
+        }
+
+        [TestMethod]
+        public void DocumentFilterSample()
+        {
+            var query = new ComplexQuery(
+                null,
+                "id eq 'moeta(42)'",
+                1,
+                null,
+                null,
+                "dev",
+                false);
+            var documentTranslator = new DocumentTranslator("test", Helpers.Enums.DocumentType.Data, "records", 3);
+            var sqlQuery = documentTranslator.Translate(query);
+            Assert.AreEqual("select top 30 c.identifier, c.robot from c where c.title = 'moeta' and c.entity = 'kotori/project' and c.instance = 'dev' ", sqlQuery);
+        }
+
+        //[TestMethod]
+        //public void ProjectSelectSample2()
+        //{
+        //    var query = new ComplexQuery(
+        //        null,
+        //        null,
+        //        30,
+        //        null,
+        //        null,
+        //        "dev",
+        //        false);
+        //    var projectTranslator = new ProjectTranslator();
+        //    var sqlQuery = projectTranslator.Translate(query);
+        //    Assert.AreEqual("select top 30 * from c where c.entity = 'kotori/project' and c.instance = 'dev' ", sqlQuery);
+        //}
+
+        //[TestMethod]
+        //public void ProjectSelectSample3()
+        //{
+        //    var query = new ComplexQuery(
+        //        "a,b,c",
+        //        null,
+        //        null,
+        //        null,
+        //        null,
+        //        "dev",
+        //        false);
+        //    var projectTranslator = new ProjectTranslator();
+        //    var sqlQuery = projectTranslator.Translate(query);
+        //    Assert.AreEqual("select c.a,c.b,c.c from c where c.entity = 'kotori/project' and c.instance = 'dev' ", sqlQuery);
+        //}
+
+        //[TestMethod]
+        //public void ProjectSelectSampleWithTransformation()
+        //{
+        //    var query = new ComplexQuery(
+        //        "",
+        //        "id eq 'yuri-yuri'",
+        //        null,
+        //        null,
+        //        null,
+        //        "dev",
+        //        false);
+        //    var projectTranslator = new ProjectTranslator();
+        //    var sqlQuery = projectTranslator.Translate(query);
+        //    Assert.AreEqual("select * from c where c.identifier = 'kotori://api/projects/yuri-yuri' and c.entity = 'kotori/project' and c.instance = 'dev' ", sqlQuery);
+        //}
     }
 }
